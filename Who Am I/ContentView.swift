e3f4b789d13c03b83b8722e740666ccf99c53e38
Ethan Humphrey <ethantheinnovator@gmail.com>
2019-10-18 07:56:37 -0400

    Initial Commit

---
+++ Who Am I/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Who Am I
+//
+//  Created by Ethan Humphrey on 10/18/19.
+//  Copyright © 2019 Ethan Humphrey. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
